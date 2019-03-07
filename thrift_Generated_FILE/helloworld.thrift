b5c626178c0cc6b6a06d56b3e629211cde4f76cd
Bilal Akbar <muhammadbilalakbar021@gmail.com>
2019-03-07 23:43:16 +0500

    Add files via upload

---
+++ thrift_Generated_FILE/helloworld.thrift
@@ -0,0 +1,24 @@
+namespace java org.cs.helloworldservice
+namespace cpp HelloWorldService
+namespace cl HelloWorldService
+namespace d HelloWorldService
+namespace dart HelloWorldService
+namespace php HelloWorldService
+namespace perl HelloWorldService
+namespace haxe HelloWorldService
+namespace netcore HelloWorldService
+
+
+struct hello{
+		1: i32 number ,
+		2: string first_Name ,
+		3: string last_Name ,
+		4: i32 age
+	}
+
+
+service HelloWorldService
+{
+	        hello geHello(),
+	
+}
